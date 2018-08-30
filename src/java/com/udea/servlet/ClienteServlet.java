4bd5e2974ed8b131b9ca3b224df7d343168169e7
juan.rojas9 <juan.rojas9@PC-LIS>
2018-08-30 15:10:09 -0500

    borra unos datos

--- src/java/com/udea/servlet/ClienteServlet.java
+++ src/java/com/udea/servlet/ClienteServlet.java
@@ -36,16 +36,7 @@ public class ClienteServlet extends HttpServlet {
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ClienteServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ClienteServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+           
         }
     }
 
--- src/java/com/udea/servlet/VehiculoServlet.java
+++ src/java/com/udea/servlet/VehiculoServlet.java
@@ -32,15 +32,7 @@ public class VehiculoServlet extends HttpServlet {
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
             /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet VehiculoServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet VehiculoServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+           
         }
     }
 
